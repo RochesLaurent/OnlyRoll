dd45bfcd554c5445b6f7d9971cfd5fcb6caeba5d
Laurent <laurent.roches.simplon@gmail.com>
2025-11-18 14:12:10 +0100

    fixe: config mysql passer en 8.0

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,12 +1,5 @@
 -- Script d'initialisation MySQL (optionnel)
 -- Exécuté au premier démarrage
 
--- Créer des utilisateurs supplémentaires si nécessaire
--- CREATE USER IF NOT EXISTS 'readonly'@'%' IDENTIFIED BY 'readonly_pass';
--- GRANT SELECT ON onlyroll.* TO 'readonly'@'%';
-
--- Configurer les performances
-SET GLOBAL max_connections = 200;
-
 -- Log
 SELECT 'MySQL initialization completed' AS status;
